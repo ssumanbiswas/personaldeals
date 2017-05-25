7cf6118a9841bea1b3f6bafcd40a676fdba2f5a0
Suman Biswas <ssumanbiswas@aol.com>
2017-05-25 09:46:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,11 @@ def makeWebhookResult(req):
         #speech = "Hey Suman, I found an excellent deal for you. Instead of your standard 1% on your AMEX blue cash card, I can offer" + str(cost[zone]) + " percent cash back for your current purchase of" + dollars + "or above with" + zone + ". This offer will expire in next 330 minutes." 
     
     if str(zone) =='BMW':
-        speech = "Hey" + str(vid1[vid]) + ", I found an excellent deal for you. Instead of your standard 1% on your AMEX blue cash card, I can offer" + str(cost[zone]) + " percent cash back for your current purchase of" + dollars + "or above with" + zone + ". This offer will expire in next 330 minutes." 
+        speech = "Hey"+ str(vid1[vid]) + ", I could offer a deal. Instead of standard "+str(cost[zone])+" on your AMEX blue cash card, I can instantly offer 6 months pro-rated installment payments on your current large purchase of 10000$ or above with" +zone+". This offer will expire in next 48 hours."
+    elif str(zone) == 'Target':
+        speech = "Hey" + str(vid1[vid]) + ", I located a deal for you. Since you are in " + zone + "Your Visa Chase freedom have" + str(cost[zone]) + "cashback on Department Stores for current Quarter.I would recommend to take advantage of it."
+    else:
+        speech = "Hey" + str(vid1[vid]) + ", I found an excellent deal based on your Card Profiles. Instead of your standard 1% on your AMEX blue cash card, I can offer" + str(cost[zone]) + " percent cash back for your current purchase of" + dollars + "or above with" + zone + ". This offer will expire in next 30 minutes." 
     print("Response:")
     print(speech)
 
