02420697b322b6c182a30562a5568aea3ce68d6a
Suman Biswas <ssumanbiswas@aol.com>
2017-05-25 10:33:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def makeWebhookResult(req):
     dollars = parameters.get("dollar")
     vid = parameters.get("VoiceIdentification")
     
-    cost = {'Air India':2, 'Delta Airlines':2.5, 'American Airlines':3, 'BestBuy':4, 'Ebay':5, 'Amazon':1.5 , 'Target':2 , 'Hilton' : 3, 'BMW' : 2.5}
+    cost = {'Air India':2, 'Delta Airlines':2.5, 'American Airlines':3, 'BestBuy':4, 'Ebay':5, 'Amazon':1.5 , 'Target':5 , 'Hilton' : 3, 'BMW' : 2.5}
     vid1 = {'0000': 'Non Amex user','8829' : 'suman' , '0001' : 'Hema', '0002' : 'prem', '0003' : 'swarup'}
     #if zone == "BMW":
         #if  parameters.get("merchant") == "BMW":
@@ -45,7 +45,7 @@ def makeWebhookResult(req):
         #speech = "Hey Suman, I found an excellent deal for you. Instead of your standard 1% on your AMEX blue cash card, I can offer" + str(cost[zone]) + " percent cash back for your current purchase of" + dollars + "or above with" + zone + ". This offer will expire in next 330 minutes." 
     
     if str(zone) =='BMW':
-        speech = "Hey"+ str(vid1[vid]) + ", I could offer a deal. Instead of standard "+str(cost[zone])+" on your AMEX blue cash card, I can instantly offer 6 months pro-rated installment payments on your current large purchase of" + dollars + " or above with" +zone+". This offer will expire in next 48 hours."
+        speech = "Hey"+ str(vid1[vid]) + ", I could offer a deal. Instead of standard "+str(cost[zone])+" on your AMEX blue cash card, I can offer 6 months pro-rated installment payments on your current large purchase of" + dollars + " or above with" +zone+". This offer will expire in next 48 hours."
     elif str(zone) == 'Target':
         speech = "Hey" + str(vid1[vid]) + ", I located a deal for you. Since you are in " + zone + "Your Visa Chase freedom have" + str(cost[zone]) + "cashback on Department Stores for current Quarter.I would recommend to take advantage of it."
     else:
